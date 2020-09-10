2f6c6926a0e66ea89a6422b3f2b00e97a8393603
Truc Phan <anhtrucphanit@gmail.com>
2020-09-10 11:21:12 +0700

    testttt

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 $token = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
 $user_id = "1254653879";
 
-/* $request_url = 'https://api.telegram.org/bot' . $token . '/getUpdates';
+$request_url = 'https://api.telegram.org/bot' . $token . '/getUpdates';
 $result = file_get_contents($request_url);
 
 debug_to_console($result);
- */
+
 $request_params = [
     'chat_id' => $user_id,
     'text' => 'Bạn là ai dị?'
 ];
-$request_url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($request_params);
-
+$request_url_2 = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($request_params);
+file_get_contents($request_url_2);
 
 debug_to_console($request_params);
 
