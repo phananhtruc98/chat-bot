ae7581b096a4557bbf0d5cb8500480dab1a21a47
Truc Phan <anhtrucphanit@gmail.com>
2020-09-10 09:33:24 +0700

    Commit

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 <?php
- $token = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
-$user_id = "-409220601";
+$token = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
+$user_id = "1254653879";
 
 $request_params = [
     'chat_id' => $user_id,
     'text' => 'Bạn là ai dị?'
 ];
-$request_params_getchat = [
-    'chat_id' => $user_id,
-    'text' => 'Bạn là ai dị?'
-];
-$request_url = 'https://api.telegram.org/bot' . $token;
-header('Content-Type: application/json');
+$request_url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($request_params);
 
-$getUpdates = $request_url . '/getUpdates';
-echo json_encode(json_decode(file_get_contents($getUpdates)), JSON_PRETTY_PRINT);
-
-$getChat = $request_url . '/getChat?' . http_build_query($request_params_getchat);
-
-echo json_encode(json_decode(file_get_contents($getChat)), JSON_PRETTY_PRINT);
-
-//$newMessage = 
-
-?> 
+file_get_contents($request_url)
+?>
