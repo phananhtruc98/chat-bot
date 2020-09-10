a960328664aa760e9d6392404e8b3ac31516bd9a
Truc Phan <anhtrucphanit@gmail.com>
2020-09-10 10:26:53 +0700

    TEST

--- index.php
+++ index.php
@@ -9,12 +9,20 @@ $botToken = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
 
         // Reply with callback_query data
         $data = http_build_query([
-            'text' => 'Selected language: ' . $update['callback_query']['data'],
+            'text' => $msg = $update['message']['text'],
             'chat_id' => $update['callback_query']['from']['id']
         ]);
+        debug_to_console($data);
         file_get_contents($botAPI . "/sendMessage?{$data}");
     }
-
+    function debug_to_console($data) {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
+    
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }
+/* 
     // Check for normal command
     $msg = $update['message']['text'];
     if ($msg === "/new_word") {
@@ -40,7 +48,5 @@ $botToken = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
         ]);
 
         // Send keyboard
-        file_get_contents($botAPI . "/sendMessage?{$data}&reply_markup={$keyboard}");
-
-
+        file_get_contents($botAPI . "/sendMessage?{$data}&reply_markup={$keyboard}"); */
 ?>
