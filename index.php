48068124ea87e88c77af5b4402e694061116cb62
Truc Phan <anhtrucphanit@gmail.com>
2020-09-10 09:55:04 +0700

    commit

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ $token = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
 //$user_id = "1254653879";
 
 
-$content = \file_get_contents('php://input');
+$content = file_get_contents('php://input');
 console.log('content',$content);
-$update = \json_decode($content, true);
+$update = json_decode($content, true);
 console.log('update',$update);
-if(!$update){
+/* if(!$update){
     exit;
 }
 $message = isset($update['message']) ? $update['message'] : null;
@@ -15,7 +15,7 @@ $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : null;
 $request_params = [
     'chat_id' => $chatId,
     'text' => $message
-];
+]; */
 console.log('message',$message);
 console.log('chatId',$chatId);
 /* \header('Content-Type: application/json');
