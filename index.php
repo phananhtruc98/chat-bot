e4f7872d154f16c8a6074e709a7ea322a86b5253
Truc Phan <anhtrucphanit@gmail.com>
2020-09-19 23:15:42 +0700

    Choose action

--- index.php
+++ index.php
@@ -1,75 +1,71 @@
 <?php
 $token = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
-
-$_POST = file_get_contents('php://input');
+$user_id = "1254653879";
 $api = 'https://api.telegram.org/bot' . $token;
 
+// $update = json_decode(file_get_contents('php://input'), true);
 
+// $a = $update['message']['text'];
+
+// $request_params = [
+//     'chat_id' => $user_id,
+//     'text' => 'hellos'
+// ];
+// $request_url = 'https://api.telegram.org/bot/' . $token . '/sendMessage?' . http_build_query($request_params);
+
+// file_get_contents($request_url);
+// ________________________________________________________________
 $content = file_get_contents("php://input");
-$update = json_decode($content, true);
-
-$callback_query = $update['callback_query'];
-$message_id = ['callback_query']['message']['message_id']; //callbackquery message id
-$chat_id = $callback_query['message']['chat']['id']; // callbackquery chat id 
-$message_text = ['callback_query']['message']['text'];
-debug_to_console($message_text);
-/* function isValidJSON($str)
-{
-    json_decode($str);
-    return json_last_error() == JSON_ERROR_NONE;
-}
-
-$json_params = file_get_contents("php://input");
-
-if (strlen($json_params) > 0 && isValidJSON($json_params))
-    $decoded_params = json_decode($json_params);
-    debug_to_console($decoded_params); */
-
-
-/* $botToken = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
-    $botAPI = "https://api.telegram.org/bot" . $botToken;
-
-    // Check if callback is set
+if ($content != null) {
+    $update = json_decode($content, true); //callbackquery message id    
     if (isset($update['callback_query'])) {
-
         // Reply with callback_query data
         $data = http_build_query([
-            'text' => $msg = $update['message']['text'],
-            'chat_id' => $update['callback_query']['from']['id']
+            'text' => $msg = $update['callback_query']['data'],
+            'chat_id' => $update['callback_query']['message']['chat']['id']
         ]);
         
-        file_get_contents($botAPI . "/sendMessage?{$data}");
-    } */
-function debug_to_console($data)
-{
-    $output = $data;
-
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
-/* 
+        file_get_contents($api . "/sendMessage?{$data}");
+    } 
+    else{
+        $chat_id = $update['message']['chat']['id']; // callbackquery chat id
+        $message_text = $update['message']['text'];
+    }
+    
     // Check for normal command
-    $msg = $update['message']['text'];
-    if ($msg === "/new_word") {
+    if ($message_text === "/new_word") {
 
         // Create keyboard
         $data = http_build_query([
-            'text' => 'Please select language;',
-            'chat_id' => $update['message']['from']['id']
+            'text' => 'Please choose your action:',
+            'chat_id' => $update['message']['chat']['id']
         ]);
         $keyboard = json_encode([
             "inline_keyboard" => [
                 [
                     [
-                        "text" => "english",
-                        "callback_data" => "english"
+                        "text" => "Add New Word",
+                        "callback_data" => "Ok friend"
                     ],
                     [
-                        "text" => "russian",
-                        "callback_data" => "russian"
+                        "text" => "Remind random word",
+                        "callback_data" => "Good job my friend"
                     ]
                 ]
             ]
         ]);
 
         // Send keyboard
-        file_get_contents($botAPI . "/sendMessage?{$data}&reply_markup={$keyboard}"); */
+        file_get_contents($api . "/sendMessage?{$data}&reply_markup={$keyboard}");
+
+
+        // $request_params = [
+        //     'chat_id' => $chat_id,
+        //     'text' => $message_text
+        // ];
+
+        // $request_url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($request_params);
+        // file_get_contents($request_url);
+    }
+}
+echo 'a';
