96b66224802da95a180a1eaf91d30aa7190b44f8
Truc Phan <anhtrucphanit@gmail.com>
2020-09-24 18:03:26 +0700

    Commit

--- index.php
+++ index.php
@@ -4,17 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use Kreait\Firebase\Factory;
 
 header('Content-Type: application/json');
-$factory = (new Factory())
-    ->withDatabaseUri('https://php-chat-bot-ed16a.firebaseio.com/');
-$database = $factory->createDatabase();
-$reference = $database->getReference('words')->getValue();
-$json_encode = json_encode($reference, JSON_PRETTY_PRINT);
-$array = json_decode($json_encode, true);
-$first_value = reset($array);
-//echo $first_value;
-$rand_key = array_rand($array, 1);
 
-$rand_word = $array[$rand_key];
 //---------------------------------------------------------------
 $token = "1263823155:AAF7VbdQ9jg04P-sIEKiyVnZ0Bd2QEqVGm4";
 $user_id = "1254653879";
@@ -25,10 +15,9 @@ if ($content != null) {
     if (isset($update['callback_query'])) {
         // Reply with callback_query data
         $data = http_build_query([
-            'text' => $msg = $update['callback_query']['data'],
+            'text' =>  $update['callback_query']['data'],
             'chat_id' => $update['callback_query']['message']['chat']['id']
         ]);
-
         file_get_contents($api . "/sendMessage?{$data}");
     } else {
         $chat_id = $update['message']['chat']['id']; // callbackquery chat id
@@ -37,18 +26,38 @@ if ($content != null) {
 
     // Check for normal command
     if ($message_text === "/random_word") {
+        $factory = (new Factory())
+            ->withDatabaseUri('https://php-chat-bot-ed16a.firebaseio.com/');
+        $database = $factory->createDatabase();
+        $reference = $database->getReference('words')->getValue();
+        $json_encode = json_encode($reference, JSON_PRETTY_PRINT);
+        $array = json_decode($json_encode, true);
+        $first_value = reset($array);
+//echo $first_value;
+        $rand_key = array_rand($array, 1);
+
+        $rand_word = $array[$rand_key];
+        $word = $rand_word['name'] . ': ' . $rand_word['meaning'];
         $data = http_build_query([
             'text' => "Please choose your action: ",
             'chat_id' => $update['message']['chat']['id']
         ]);
-
-        $keyboard = array(
-            "inline_keyboard" => array(array(array("text" => "Remind random word", "callback_data" => $rand_word['name'] . ': ' . $rand_word['meaning'])))
-        );
+        $keyboard = json_encode([
+            "inline_keyboard" => [
+                [
+                    [
+                        "text" => "All words",
+                        "callback_data" => "Update later"
+                    ],
+                    [
+                        "text" => "Give random word",
+                        "callback_data" => $word
+                    ]
+                ]
+            ]
+        ]);
         // Send keyboard
         file_get_contents($api . "/sendMessage?{$data}&reply_markup={$keyboard}");
     }
 }
-
-
 ?>
